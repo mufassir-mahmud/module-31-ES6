eb8da80c7c3e87eed5ba4f1b0ab87eb946f3b092
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-01 19:53:31 +0600

    find

---
+++ find.js
@@ -0,0 +1,10 @@
+const numbers = [41,52,33,44,55,26,17];
+const selected = numbers.find(number => number > 50);
+console.log(selected);
+
+/**
+ * 1.map
+ * 2.filter
+ * 3.forEach
+ * 4.find
+ * */ 
