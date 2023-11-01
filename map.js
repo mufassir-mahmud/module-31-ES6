40656b7ebb885e6dd5e05f2f3abdc6bc9cb74d97
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-01 09:42:43 +0600

    map

---
+++ map.js
@@ -0,0 +1,23 @@
+const numbers = [2,4,6,8,10]
+// const doubled = [];
+// for( const num of numbers){
+//     const double = num * 2;
+//     doubled.push(double)
+// }
+// console.log(doubled);
+// function doubled(num){
+//     console.log('Num Now', num);
+//     return num * 2
+// }
+// const result = numbers.map(doubled);
+// console.log(result);
+// function doubled(num){
+//     return num*2;
+// }
+// const result = numbers.map(doubled)
+// console.log(result);
+// const double = n => n*2;
+// const result = numbers.map(double);
+// console.log(result);
+const result = numbers.map(n => n*2);
+console.log(result);
