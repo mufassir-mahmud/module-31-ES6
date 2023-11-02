7ae0ccf3e82e9628687ac87d77ff48a6f81a8de8
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2023-11-02 20:37:53 +0600

    dot

---
+++ dot.js
@@ -0,0 +1,8 @@
+const person = {
+    name: 'mufassir mahmud',
+    job: 'junior developer'
+}
+const name = 'name'
+console.log(person.job);
+console.log(person['job']);
+console.log(person[name]);
